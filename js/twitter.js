19f65e575dab26e34f60b0c268203e1b3e61665d
Sahil <sahildua2305@gmail.com>
2014-12-10 04:52:37 +0530

    Initial commit

---
+++ js/twitter.js
@@ -0,0 +1,8 @@
+var d=document.createElement("div");
+d.className="action-pocket-container";
+var e=document.createElement("a");
+e.setAttribute("class","ProfileTweet-action js-tooltip");
+e.setAttribute("href","#");
+e.setAttribute("data-original-title","Save tweet for later");
+var f=document.createElement("span");
+return f.setAttribute("class","icon icon-pocket"),f.setAttribute("style","background-image: url(https://d2jjx1cmnmp22i.cloudfront.net/safari/twttr-sprite@"+(o?"2x":"1x")+".png) !important;"),$(e).append(f),$(d).append(e),d;
