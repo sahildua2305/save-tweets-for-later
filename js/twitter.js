8875f6a54a5f1f736646d89b2856945de0a74ffb
Sahil <sahildua2305@gmail.com>
2014-12-10 14:10:47 +0530

    placing button on every new tweet(working on it)

--- js/twitter.js
+++ js/twitter.js
@@ -1,8 +1,38 @@
-var d=document.createElement("div");
-d.className="action-pocket-container";
-var e=document.createElement("a");
-e.setAttribute("class","ProfileTweet-action js-tooltip");
-e.setAttribute("href","#");
-e.setAttribute("data-original-title","Save tweet for later");
-var f=document.createElement("span");
-return f.setAttribute("class","icon icon-pocket"),f.setAttribute("style","background-image: url(https://d2jjx1cmnmp22i.cloudfront.net/safari/twttr-sprite@"+(o?"2x":"1x")+".png) !important;"),$(e).append(f),$(d).append(e),d;
+
+function create(){
+	var d=document.createElement("div");
+	d.className="action-save-container";
+	var e=document.createElement("a");
+	e.setAttribute("class","ProfileTweet-action js-tooltip");
+	e.setAttribute("href","#");
+	e.setAttribute("data-original-title","Save tweet for later");
+	var o=0;
+	var f=document.createElement("span");
+	f.setAttribute("class","icon icon-save");
+	f.setAttribute("style","background-image: url(https://d2jjx1cmnmp22i.cloudfront.net/safari/twttr-sprite@"+(o?"2x":"1x")+".png) !important;");
+	$(e).append(f);
+	$(d).append(e);
+	var tweets = $(".tweet");
+	for(var i=0;i<tweets.length;i++){
+		if(!$(tweets[i]).has(".action-save-container").length){
+			console.log("already has");
+		}
+		else{
+			console.log("needs to be added");
+		}
+	}
+	//$(d).appendTo(new_tweets);
+}
+//create();
+
+var refreshInterval = 1050;
+function exp(){
+	var e = document.createElement("p");
+	e.className = "experiments";
+	var tweet = document.getElementsByClassName('tweet');
+	//console.log(tweet);
+	$("<p>testing</p>").appendTo(".tweet");
+}
+//exp();
+
+setInterval(function(){create();},1000);
