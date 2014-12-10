6b1e8fbc1019ea6528b1d88f7bfa5f58bddb95e5
Sahil <sahildua2305@gmail.com>
2014-12-11 01:34:04 +0530

    Working on fetching tweet's link after click on save button

--- js/twitter.js
+++ js/twitter.js
@@ -1,15 +1,17 @@
-
+	
 function create(){
 	var d=document.createElement("div");
 	d.className="action-save-container";
 	var e=document.createElement("a");
 	e.setAttribute("class","ProfileTweet-action js-tooltip");
+	e.setAttribute("id","save-link");
 	e.setAttribute("href","#");
 	e.setAttribute("data-original-title","Save tweet for later");
 	var o=0;
 	var f=document.createElement("span");
 	f.setAttribute("class","icon icon-save");
 	f.setAttribute("id","save-extension");
+	//f.setAttribute("onclick","saveTweet()");
 	f.setAttribute("style","background-image: url(https://pbs.twimg.com/media/B4fddhjCIAEgDy8.png) !important;");
 	$(e).append(f);
 	$(d).append(e);
@@ -25,6 +27,24 @@ function create(){
 var refreshInterval = 1500;
 setInterval(function(){create();},refreshInterval);
 
-$("#save-extension").click(function(){
-	console.log("s");
+$(document).ready(function(){
+	var e = document.getElementsByClassName("ProfileTweet-action");
+	
+	function saveTweet(e){
+		var tweetLink = $(e).parent();
+		tweetLink = tweetLink.parent();
+		tweetLink = tweetLink.parent();
+		tweetLink = tweetLink.find(".tweet-timestamp");
+		console.log(tweetLink);
+		console.log("recorded");
+	}
+	
+	$(e).click(function(e){
+		//var tweetLink = $(e).parent().parent().parent().find(".tweet-timestamp").attr('href');
+		//var tweetLink = e.parents("div.tweet").find(".tweet-timestamp").attr("href");
+		saveTweet(e);
+		//var tweetLink = e.parentElement.nodeName;
+		//console.log(tweetLink);
+		return false;
+	});
 });
